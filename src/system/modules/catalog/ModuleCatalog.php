e3994c4e6aef15a883ef03ff48c9cff639f9829e
Christian Schiffler <c.schiffler@cyberspectrum.de>
2010-01-30 09:00:56 +0000

    improved the fix as now it was causing warnings due to empty array. Now the subarrays are added empty aswell.
    git-svn-id: http://typolight-catalog.googlecode.com/svn@104 5f8f583f-444c-0410-842f-6b1beb028a02

--- src/system/modules/catalog/ModuleCatalog.php
+++ src/system/modules/catalog/ModuleCatalog.php
@@ -2308,7 +2308,7 @@ abstract class ModuleCatalog extends Module
 		$files = deserialize($files,true);
 		if (!is_array($files) || count($files) < 1)
 		{
-			return array();
+			return array('files'=>array(),'src'=>array(),'html'=>array());
 		}	
 
 		// required for parseMetaFile function (in FrontEnd)
